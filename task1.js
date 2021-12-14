79f6e4d0a084be727d01626f93990558ba52e3e6
abdessalem1998 <89970442+abdessalem1998@users.noreply.github.com>
2021-12-14 21:54:44 +0100

    Upadte stringLength to Es6 form and throw an error if  The word length is less then 1 or more than 10

--- task1.js
+++ task1.js
@@ -1,4 +1,9 @@
-function stringLength(str){
+const stringLength = (str) => {
   return str.length;
+  if (str.length < 1 || str.length > 10) {
+    throw Error('The word length must be at least 1 character long and not longer than 10 characters');
+  } else {
+    return str.length;
+  }
 }
 module.exports = stringLength;
