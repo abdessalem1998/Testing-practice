1e06dd8b18a93309300bd441044ac413cf32f718
abdessalem1998 <89970442+abdessalem1998@users.noreply.github.com>
2021-12-14 22:08:49 +0100

    add tests for the new updated functopn to check if the string length is more than 0 and less then 11

--- task1.test.js
+++ task1.test.js
@@ -2,11 +2,27 @@ const stringLength = require('./task1');
 
 test('insert "hello"  to equal 5', () => {
   //Arrange
-  const input = "hello";
+  const input = 'hello';
 
   //Act
-  const output = stringLength('hello');
+  const output = stringLength(input);
 
   //Assert
   expect(output).toBe(5);
+});
+
+test('The length of the input should not exceed 10 characters', () => {
+  const input = 'hellohello';
+
+  const output = stringLength(input);
+
+  expect(output).not.toBeGreaterThan(10);
+});
+
+test('The length of the input should not be less than 1 character', () => {
+  const input = 'h';
+
+  const output = stringLength(input);
+
+  expect(output).not.toBeLessThan(1);
 });
