38616d969bda6f6b0812fb2b949fe7fb5973652a
abdessalem1998 <89970442+abdessalem1998@users.noreply.github.com>
2021-12-15 12:38:41 +0100

    Writing a test for a capitalize(string) function

---
+++ task4.test.js
@@ -0,0 +1,21 @@
+test('Capitalized string', () => {
+  //Arrange
+  const input = 'hello';
+
+  //Act
+  const output = capitalize(input);
+
+  //Assert
+  expect(output).toEqual('Hello');
+});
+
+test('Capitalized string', () => {
+  //Arrange
+  const input = 's';
+
+  //Act
+  const output = capitalize(input);
+
+  //Assert
+  expect(output).toEqual('S');
+});
